38c13c04b200091ce3a027ad51919a55f7561293
somashekar gouda <somashekar.patil09@gmail.com>
2023-04-25 15:20:35 +0530

    Delete Store.jsx

--- src/redux/Store.jsx
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddlwware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./Reducers";
-
-const store = createStore(rootReducer, applyMiddlwware(thunk));
-
-export default store;
