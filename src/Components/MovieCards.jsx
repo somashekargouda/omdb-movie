c9db09e08e7bb0f52fa3b022c9989b4ecf7257f8
somashekar gouda <somashekar.patil09@gmail.com>
2023-04-25 11:22:47 +0530

    Add files via upload

--- src/Components/MovieCards.jsx
+++ src/Components/MovieCards.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const MovieCards = (props) => {
+const MovieCards = (props) => {l
+  console.log(props);
   return (
-    <div className="card">
-      <img src={props?.Poster} />
-      <h2>Movie name : {props.Title}</h2>
-      <h3>Year of release : {props.Year}</h3>
-      <h3>Type : {props.Type}</h3>
-    </div>
+    <Card sx={{ padding: "10px", marginTop: "10px", width: "280px" }}>
+      <CardMedia sx={{ height: 190 }} image={props.Poster} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.Title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.Type}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.Year}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to watchlist</Button>
+      </CardActions>
+    </Card>
   );
 };
 
